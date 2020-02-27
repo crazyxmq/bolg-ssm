90ce40b3547e218e2884a0235c2a7239dc201a91
bingo <956561101@qq.com>
2020-02-27 21:08:18 +0800

    -- 测试后台主页

--- src/main/java/com/xmq/admin/controller/SysUserController.java
+++ src/main/java/com/xmq/admin/controller/SysUserController.java
@@ -1,8 +1,21 @@
 package com.xmq.admin.controller;
 
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /*
-* 后台用户的类
-* */
+ * 后台用户的类
+ * */
+
+@Controller
 public class SysUserController {
 
+
+    @RequestMapping("/main")
+    public String toMain(){
+        return "index";
+    }
 }
